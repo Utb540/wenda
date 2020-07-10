120c5713f149c1cdb27c9dcdd4693e0da7fe203d
zhaodi <zasxzhao@qq.com>
2020-07-10 15:51:02 +0800

    init a wenda project

---
+++ src/test/java/cn/zhaodi/wenda/WendaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zhaodi.wenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WendaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
